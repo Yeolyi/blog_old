9b8af6daa93a4a51edfdcb0a143da494e2588ade
Yeolyi <type101315@gmail.com>
2021-11-16 23:42:53 +0900

    홈화면 내용 보완

--- js/frontScroll.js
+++ js/frontScroll.js
@@ -1,5 +1,5 @@
 class ScrollBlock {
-    constructor(startH, fadingH, endH, appearAnim, disappearAnim) {
+    constructor(startH, fadingH, endH, appearAnim, disappearAnim, moveLen=1) {
         this.startH = startH;
         this.fadingH = fadingH;
         this.endH = endH;
@@ -12,16 +12,17 @@ class ScrollBlock {
             }
 
             function defaultMove(height) {
-                return easeInOut(startH, Math.min(startH+1000, endH), height, 0, 0.5);
+                // return (height-startH)/200;
+                return easeInOut(startH, endH, height, 0, moveLen);
             }
             if (ret.hasOwnProperty("padding-bottom")) {
-                ret["padding-bottom"] += defaultMove(height)
+                ret["padding-bottom"] = ret["padding-bottom"] + defaultMove(height)
             } else {
                 ret["padding-bottom"] = defaultMove(height)
             }
             for (let key in ret) {
-                if (key == "padding-bottom") {
-                    ret[key] += "em";
+                if (key == "padding-bottom" || key == "padding-top") {
+                    ret[key] += "rem";
                 }
             }
             target.css(ret)
@@ -38,126 +39,123 @@ function easeInOut(startH, endH, curH, startVal, endVal) {
     return startVal + (endVal - startVal) * easeInOutQuart(p)
 }
 
-const scrollBlockOne = new ScrollBlock(0, 600, 1000,
+
+function frontPageTitleObject(startH, endH, bottomPadding=0) {
+    return new ScrollBlock(startH, endH - 400, endH,
+        function (height) {
+            popPadding = easeInOut(startH, startH + 300, height, 0, 2.5)
+            newOpacity = easeInOut(startH, startH + 300, height, 0, 1)
+            return {
+                "padding-bottom": (popPadding + bottomPadding),
+                "opacity": newOpacity
+            }
+        },
+        function (height) {
+            newOpacity = easeInOut(endH - 400, endH, height, 1, 0)
+            return {
+                "padding-bottom": (2.5 + bottomPadding),
+                "opacity": newOpacity
+            }
+        },
+        false
+    )
+}
+
+/* 앱 */
+
+function frontPageNoPopup(startH, topPadding=0, bottomPadding=0, move=true) {
+    return new ScrollBlock(startH, startH + 300, startH + 600,
+        function (height) {
+            newOpacity = easeInOut(startH, startH + 300, height, 0, 1)
+            return {
+                "padding-top": topPadding,
+                "padding-bottom": bottomPadding,
+                "opacity": newOpacity
+            }
+        },
+        function (height) {
+            newOpacity = easeInOut(startH + 300, startH + 600, height, 1, 0)
+            return {
+                "padding-top": topPadding,
+                "padding-bottom": bottomPadding,
+                "opacity": newOpacity
+            }
+        },
+        move
+    )
+}
+
+const hello = new ScrollBlock(0, 2700, 3000,
     function (height) {
-        popPadding = easeInOut(0, 300, height, 0, 2.2)
+        popPadding = easeInOut(0, 300, height, 0, 5)
         newOpacity = easeInOut(0, 300, height, 0.3, 1)
         return {
-            "padding-top": "1.25em",
             "padding-bottom": popPadding,
             "opacity": newOpacity
         }
     },
     function (height) {
-        newOpacity = easeInOut(600, 1000, height, 1, 0)
+        newOpacity = easeInOut(2700, 3000, height, 1, 0)
         return {
-            "padding-top": "1.25em",
-            "padding-bottom": 2.2,
-            "opacity": newOpacity
-        }
-    }
-)
-
-const scrollBlockTwo = new ScrollBlock(0, 600, 1000,
-    function (height) {
-        newOpacity = easeInOut(0, 600, height, 0, 1)
-        return {
-            "padding-top": "1.25em",
+            "padding-bottom": 5,
             "opacity": newOpacity
         }
     },
+    false
+)
+const iamSY = new ScrollBlock(0, 2700, 3000,
     function (height) {
-        newOpacity = easeInOut(600, 1000, height, 1, 0)
+        newOpacity = easeInOut(0, 300, height, 0, 1)
+        if (height < 300) {
+            return {
+                "opacity": newOpacity
+            }
+        } else if (height < 2400) {
+            return {
+                "padding-top": easeInOut(300, 600, height, 0, 5)
+            }
+        } else {
+            return {
+                "padding-top": 5 - easeInOut(2400, 2700, height, 0, 5)
+            }
+        }
+    },
+    function (height) {
+        newOpacity = easeInOut(2700, 3000, height, 1, 0)
         return {
-            "padding-top": "1.25em",
             "opacity": newOpacity
         }
-    }
+    },
+    false
 )
-  
 
-function frontPageTitleObject(startH, endH) {
-    return new ScrollBlock(startH, endH-400, endH,
-        function (height) {
-            popPadding = easeInOut(startH, startH+300, height, 0, 1.8)
-            newOpacity = easeInOut(startH, startH+300, height, 0, 1)
-            return {
-                "padding-bottom": popPadding+2,
-                "opacity": newOpacity
-            }
-        },
-        function (height) {
-            newOpacity = easeInOut(endH-400, endH, height, 1, 0)
-            return {
-                "padding-bottom": 1.8+2,
-                "opacity": newOpacity
-            }
-        }
-    )
-}
-
-const scrollBlockThree = frontPageTitleObject(900, 3000)
-
-/* 앱 */
-
-function frontPageAppTitleObject(startH, offset) {
-    return new ScrollBlock(startH, startH+600, startH+1000,
-        function (height) {
-            newOpacity = easeInOut(startH, startH+300, height, 0, 1)
-            ret =  {
-                "padding-bottom": 2,
-                "opacity": newOpacity
-            }
-            if (offset) {
-                ret["padding-top"] = offset+"em"
-            }
-            return ret
-        },
-        function (height) {
-            newOpacity = easeInOut(startH+600, startH+1000, height, 1, 0)
-            ret = {
-                "padding-bottom": 2,
-                "opacity": newOpacity
-            }
-            if (offset) {
-                ret["padding-top"] = offset+"em"
-            }
-            return ret
-        }
-    )
-}
-
-function frontPageAppIconObject(startH) {
-    return new ScrollBlock(startH, startH+600, startH+1000,
-        function (height) {
-            newOpacity = easeInOut(startH, startH+600, height, 0, 1)
-            return {
-                "padding-top": "15vw",
-                "opacity": newOpacity
-            }
-        },
-        function (height) {
-            newOpacity = easeInOut(startH+600, startH+1000, height, 1, 0)
-            return {
-                "padding-top": "15vw",
-                "opacity": newOpacity
-            }
-        }
-    )
-}
+const firstIntro = frontPageNoPopup(600, 0.15, undefined, 0.3);
+const secondIntro = frontPageNoPopup(1100, 0.15, undefined, 0.3);
+const thirdIntro = frontPageNoPopup(1600, 0.15, undefined, 0.3);
+const fourthIntro = frontPageNoPopup(2100, 0.15, undefined, 0.3);
 
 /* 앱 소개 */
-const scrollBlockFour = frontPageAppTitleObject(1000)
-const scrollBlockFive = frontPageAppIconObject(1000)
-const scrollBlock6 = frontPageAppTitleObject(2000)
-const scrollBlock7 = frontPageAppIconObject(2000)
+const iOSStudying = frontPageTitleObject(2800, 4400, 8)
+const snuyum = frontPageNoPopup(3000, 0, 3)
+const scrollBlockFive = frontPageNoPopup(3000, 4, 0)
+const scrollBlock6 = frontPageNoPopup(3600, 0, 3)
+const scrollBlock7 = frontPageNoPopup(3600, 4, 0)
 /* 프론트엔드 */
-const scrollBlock8 = frontPageTitleObject(2900, 3900)
-const scrollBlock9 = frontPageAppTitleObject(3000, 2)
+const scrollBlock8 = frontPageTitleObject(4200, 5000, 8)
+const scrollBlock9 = frontPageNoPopup(4200)
 /* 마무리 */
-const scrollBlock10 = frontPageAppTitleObject(3900, 2)
+const scrollBlock10 = new ScrollBlock(5000, 100000, 100000,
+    function (height) {
+        newOpacity = easeInOut(5000, 5300, height, 0.3, 1)
+        return {
+            "opacity": newOpacity
+        }
+    },
+    function (height) { },
+    false
+)
 
-const scrollBlocks = [scrollBlockOne, scrollBlockTwo, scrollBlockThree, scrollBlockFour, scrollBlockFive, scrollBlock6, scrollBlock7, scrollBlock8, scrollBlock9, scrollBlock10]
+const scrollBlocks = [hello, iamSY, firstIntro, secondIntro, thirdIntro, fourthIntro, iOSStudying, snuyum, scrollBlockFive, scrollBlock6, scrollBlock7, scrollBlock8, scrollBlock9, scrollBlock10]
 
 function update() {
     let height = Math.max(0, document.documentElement.scrollTop);
