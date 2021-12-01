e447a5ad5914e61ede861ef7cc3414b20ea5e517
Yeolyi <type101315@gmail.com>
2021-12-01 16:34:55 +0900

    publish

--- dict/test.c
+++ dict/test.c
@@ -1,226 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <pthread.h>
 #include <stdbool.h>
 
-typedef struct _Color
+int sum = 0;
+
+int turn;
+int flag[2];
+
+void *producer(void *param)
 {
-    int key;
-    char *name;
-} Color;
-
-typedef Color Data;
-
-typedef struct _BTreeNode
-{
-    Data data;
-    struct _BTreeNode *left;
-    struct _BTreeNode *right;
-} BTreeNode;
-
-BTreeNode *MakeBTreeNode()
-{
-    BTreeNode *node = malloc(sizeof(BTreeNode));
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
-
-Data GetData(BTreeNode *bt)
-{
-    return bt->data;
-}
-
-void SetData(BTreeNode *bt, Data data)
-{
-    bt->data = data;
-}
-
-BTreeNode *GetLeftSubTree(BTreeNode *bt)
-{
-    return bt->left;
-}
-
-BTreeNode *GetRightSubTree(BTreeNode *bt)
-{
-    return bt->right;
-}
-
-void PostOrderTraverse(BTreeNode *main, void (*Transform)(BTreeNode *))
-{
-    if (!(main))
-        return;
-    PostOrderTraverse(main->left, Transform);
-    PostOrderTraverse(main->right, Transform);
-    Transform(main);
-}
-
-void FreeTreeNode(BTreeNode *main)
-{
-    free(main);
-}
-
-void DeleteTree(BTreeNode *main)
-{
-    PostOrderTraverse(main, FreeTreeNode);
-}
-
-void MakeLeftSubTree(BTreeNode *main, BTreeNode *sub)
-{
-    main->left = sub;
-}
-
-void MakeRightSubTree(BTreeNode *main, BTreeNode *sub)
-{
-    main->right = sub;
-}
-
-void BSTInit(BTreeNode **pRoot)
-{
-    *pRoot = NULL;
-}
-
-void BSTInsert(BTreeNode **pRoot, Data data)
-{
-    BTreeNode *cur = *pRoot;
-    BTreeNode *newNode = MakeBTreeNode();
-    newNode->data = data;
-
-    if (!cur)
+    for (int i = 0; i < 10000; i++)
     {
-        *pRoot = MakeBTreeNode();
-        (*pRoot)->data = data;
-        return;
+        // entry section
+        flag[0] = true;
+        turn = 1;
+        while (flag[1] && turn == 1)
+            ;
+
+        // critical section
+        sum++;
+
+        //exit section
+        flag[0] = false;
     }
-    while (true)
-    {
-        if (data.key < cur->data.key)
-        {
-            if (cur->left)
-                cur = cur->left;
-            else
-            {
-                MakeLeftSubTree(cur, newNode);
-                return;
-            }
-        }
-        else
-        {
-            if (cur->right)
-                cur = cur->right;
-            else
-            {
-                MakeRightSubTree(cur, newNode);
-                return;
-            }
-        }
-    }
+    pthread_exit(0);
 }
 
-//NULL때문에 포인터 쓰는 것도 있는건가
-BTreeNode *BSTSearch(BTreeNode *cur, int key)
+void *consumer(void *param)
 {
-    while (true)
+    for (int i = 0; i < 10000; i++)
     {
-        if (!cur)
-            return NULL;
-        if (cur->data.key == key)
-            return cur;
-        if (cur->data.key > key)
-            cur = cur->left;
-        else
-            cur = cur->right;
+        // entry section
+        flag[1] = true;
+        turn = 0;
+        while (flag[0] && turn == 0)
+            ;
+
+        // critical section
+        sum--;
+
+        //exit section
+        flag[1] = false;
     }
-}
-
-void InorderTraverse(BTreeNode *node)
-{
-    if (node->left)
-        InorderTraverse(node->left);
-    printf("%s\n", node->data.name);
-    if (node->right)
-        InorderTraverse(node->right);
-}
-
-void BSTRemove(BTreeNode **pRoot, int key)
-{
-    BTreeNode *tempNode = MakeBTreeNode();
-    BTreeNode *parent = tempNode;
-    BTreeNode *cur = *pRoot;
-
-    tempNode->right = *pRoot;
-
-    while (cur && (cur->data.key != key))
-    {
-        parent = cur;
-        if (cur->data.key < key)
-            cur = cur->right;
-        else
-            cur = cur->left;
-    }
-
-    if (!cur)
-        return;
-
-    if (cur->left && cur->right)
-    {
-        BTreeNode *altNode = cur->right;
-        BTreeNode *altNodeParent = cur;
-        while (altNode->left)
-        {
-            altNodeParent = altNode;
-            altNode = altNode->left;
-        }
-        cur->data = altNode->data;
-        if (altNodeParent->left == altNode)
-            altNodeParent->left = altNode->right;
-        else
-            altNodeParent->right = altNode->right;
-        free(altNode);
-    }
-    else if (cur->left || cur->right)
-    {
-        BTreeNode *curChild;
-        if (cur->left)
-            curChild = cur->left;
-        else
-            curChild = cur->right;
-        if (parent->left == cur)
-            parent->left = curChild;
-        else
-            parent->right = curChild;
-        free(cur);
-    }
-    else
-    {
-        if (parent->left == cur)
-            parent->left = NULL;
-        else
-            parent->right = NULL;
-        free(cur);
-    }
-    if (tempNode->right != *pRoot)
-        *pRoot = tempNode->right;
-    free(tempNode);
-    // free(cur); 애는 지우면 안되지,,,^^
+    pthread_exit(0);
 }
 
 int main()
 {
-    char *colorList[] = {"Red", "Orange", "Yellow", "Green", "Blue"};
-    int insertOrder[] = {3, 2, 0, 4, 1};
-    BTreeNode *bstRoot;
-    BSTInit(&bstRoot);
-    Data data;
-    for (int i = 0; i < 5; i++)
-    {
-        int order = insertOrder[i];
-        data.key = order;
-        data.name = colorList[order];
-        BSTInsert(&bstRoot, data);
-    }
-    BSTRemove(&bstRoot, 4);
-    BSTRemove(&bstRoot, 0);
-    BSTRemove(&bstRoot, 1);
-    InorderTraverse(bstRoot);
-    return 0;
+    pthread_t tid1, tid2;
+    pthread_create(&tid1, NULL, producer, NULL);
+    pthread_create(&tid2, NULL, consumer, NULL);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    printf("sum= %d\n", sum);
 }
